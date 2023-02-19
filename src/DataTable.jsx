0ace52cd3412172b65a351659006b72bd0c0ad49
lastnamehurt <clhurt1@gmail.com>
2023-02-19 14:41:19 -0800

    add cors

--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -97,7 +97,7 @@ const DataTable = () => {
   }, [data]);
 
   useEffect(() => {
-    fetch("https://your-seattle-plug.herokuapp.com/api/deals")
+    fetch("https://your-seattle-plug.herokuapp.com/api/deals", { mode: "cors" })
       .then((response) => response.json())
       .then((data) => {
         setData(data);
